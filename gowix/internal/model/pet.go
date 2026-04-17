ab7601e2c2db9c074e0283de0d5e8d1e943c696a
Yuyu084 <150607089+Yuyu084@users.noreply.github.com>
2026-04-17 07:13:36 +0500

    pet.go

--- gowix/internal/model/pet.go
+++ gowix/internal/model/pet.go
@@ -1,33 +1,29 @@
 package model
 
+import "time"
+
 type Pets struct {
-	Pets []Pet `json:"pets"`
+Pets []Pet `json:"pets"`
 }
-
 type Pet struct {
-	Name           string            `json:"name"`
-	Type           PetType           `json:"type"`
-	PropertyHealth PetPropertyHealth `json:"property_health"`
-	PropertyEnergy PetPropertyEnergy `json:"property_energy"`
-	Mood           PetMood           `json:"mood"`
+Name           string            `json:"name"`
+Type           PetType           `json:"type"`
+PropertyHealth PetPropertyHealth `json:"property_health"`
+PropertyEnergy PetPropertyEnergy `json:"property_energy"`
+Mood           PetMood           `json:"mood"`
+LastPlayed     time.Time         `json:"last_played"` 
 }
-
 type PetType int
-
 const (
-	PetTypeCat PetType = iota
-	PetTypeDog
+PetTypeCat PetType = iota
+PetTypeDog
 )
-
-func (p *Pet) GetMoodEmoji() string {
-	return p.Mood.GetMoodEmoji()
-}
 func (p *Pet) UpdateMoodByHealth() {
-	if p.PropertyHealth.Value <= 30 {
-		p.Mood.SetMood(MoodAngry)
-	} else if p.PropertyHealth.Value < 45 {
-		p.Mood.SetMood(MoodSad)
-	} else {
-		p.Mood.SetMood(MoodHappy)
-	}
+if p.PropertyHealth.Value <= 30 {
+p.Mood.SetMood(MoodAngry)
+} else if p.PropertyHealth.Value < 45 {
+p.Mood.SetMood(MoodSad)
+} else {
+p.Mood.SetMood(MoodHappy)
+}
 }
