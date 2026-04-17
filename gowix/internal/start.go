07b02f56cb050dd470df79c61f8b47b86e1d0e6a
Yuyu084 <150607089+Yuyu084@users.noreply.github.com>
2026-04-17 07:26:04 +0500

    start.go

--- gowix/internal/start.go
+++ gowix/internal/start.go
@@ -1,85 +1,98 @@
 package internal
 
 import (
-	// "encoding/json"
-	"flufiz/internal/model"
-	"flufiz/internal/storage"
-	"fmt"
-	"time"
-	// "os"
-	// "strconv"
+// "encoding/json"
+"flufiz/internal/model"
+"flufiz/internal/storage"
+"fmt"
+"time"
+// "os"
+// "strconv"
 )
 
 func Start() error {
-	StartPlay()
-
-	existingPets, err := Decode()
-	var pet *model.Pet
-	if err != nil || len(existingPets.Pets) == 0 {
-		pet = ProfilUser()
-	} else {
-		pet = &existingPets.Pets[0]
-	}
-
-	mainMenu := NewMainMenu(pet, "internal/model/pets.json")
-	mainMenu.Run()
-
-	// Pets := model.Pets{
-	// 	Pets: []model.Pet{
-	// 		{
-	// 			Name:           "",
-	// 			Type:           model.PetTypeCat,
-	// 			PropertyHealth: model.PetPropertyHealth{},
-	// 		},
-	// 	},
-	// }
-	// jsonData, err := json.Marshal(Pets)
-	// if err != nil {
-	// 	fmt.Println("Ошибка", err)
-	// }
-	// fmt.Println(string(jsonData))
-	// file, err := os.Create("internal/model/pets.json")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer file.Close()
-	// encoder := json.NewEncoder(file)
-	// if err := encoder.Encode(Pets); err != nil {
-	// 	panic(err)
-	// }
-
-	// decodedPets, err := Decode()
-	// if err != nil {
-	// 	return err
-	// }
-
-	// for _, pet := range decodedPets.Pets {
-	// 	fmt.Println("Имя питомца " + pet.Name)
-	// 	fmt.Printf("Тип питомца: %d\n", pet.Type)
-	// 	fmt.Println("Характеристики" + strconv.Itoa(pet.PropertyHealth.GetCurrentValue()))
-	// }
-
-	return nil
+StartPlay()
+existingPets, err := Decode()
+var pet *model.Pet
+if err != nil || len(existingPets.Pets) == 0 {
+pet = ProfilUser()
+// Сохраняем нового питомца в JSON
+pets := &model.Pets{Pets: []model.Pet{*pet}}
+storage.UpdatePetInfo(pets, storage.GetPetsFilePath())
+} else {
+pet = &existingPets.Pets[0]
+// Обновляем сонливость при запуске (если долго не играли)
+storage.AutoUpdateSleepiness(pet.Name, storage.GetPetsFilePath())
+// Обновляем здоровье при запуске (если долго не играли)
+storage.AutoUpdatePetStats(pet.Name, storage.GetPetsFilePath())
+// Перезагружаем питомца после обновлений
+updatedPets, _ := storage.LoadPetsFromFile(storage.GetPetsFilePath())
+if len(updatedPets.Pets) > 0 {
+pet = &updatedPets.Pets[0]
 }
+}
+
+mainMenu := NewMainMenu(pet, "internal/model/pets.json")
+mainMenu.Run()
+
+// Pets := model.Pets{
+//     Pets: []model.Pet{
+//         {
+//             Name:           "",
+//             Type:           model.PetTypeCat,
+//             PropertyHealth: model.PetPropertyHealth{},
+//         },
+//     },
+// }
+// jsonData, err := json.Marshal(Pets)
+// if err != nil {
+//     fmt.Println("Ошибка", err)
+// }
+// fmt.Println(string(jsonData))
+// file, err := os.Create("internal/model/pets.json")
+// if err != nil {
+//     panic(err)
+// }
+// defer file.Close()
+// encoder := json.NewEncoder(file)
+// if err := encoder.Encode(Pets); err != nil {
+//     panic(err)
+// }
+
+// decodedPets, err := Decode()
+// if err != nil {
+//     return err
+// }
+
+// for _, pet := range decodedPets.Pets {
+//     fmt.Println("Имя питомца " + pet.Name)
+//     fmt.Printf("Тип питомца: %d\n", pet.Type)
+//     fmt.Println("Характеристики" + strconv.Itoa(pet.PropertyHealth.GetCurrentValue()))
+// }
+
+return nil
+}
+
 func CreateNewPet(name string, petType model.PetType) *model.Pet {
-	newPet := &model.Pet{
-		Name:           name,
-		Type:           petType,
-		PropertyHealth: model.PetPropertyHealth{Value: 80, LastUpdated: time.Now()},
-		PropertyEnergy: model.PetPropertyEnergy{Value: 80, LastUpdated: time.Now()},
-		Mood:           model.PetMood{Mood: model.MoodHappy, LastUpdated: time.Now()},
-	}
+newPet := &model.Pet{
+Name:           name,
+Type:           petType,
+PropertyHealth: model.PetPropertyHealth{Value: 80, LastUpdated: time.Now()},
+PropertyEnergy: model.PetPropertyEnergy{Value: 80, LastUpdated: time.Now()},
+Mood:           model.PetMood{Mood: model.MoodHappy, LastUpdated: time.Now()},
+LastPlayed:     time.Now(),
+}
 
-	pets := &model.Pets{Pets: []model.Pet{*newPet}}
-	storage.UpdatePetInfo(pets, storage.GetPetsFilePath())
+pets := &model.Pets{Pets: []model.Pet{*newPet}}
+storage.UpdatePetInfo(pets, storage.GetPetsFilePath())
 
-	return newPet
+return newPet
 }
 
 func GetFirstPet() (*model.Pet, error) {
-	pets, err := storage.LoadPetsFromFile(storage.GetPetsFilePath())
-	if err != nil || len(pets.Pets) == 0 {
-		return nil, fmt.Errorf("питомец не найден")
-	}
-	return &pets.Pets[0], nil
+pets, err := storage.LoadPetsFromFile(storage.GetPetsFilePath())
+if err != nil || len(pets.Pets) == 0 {
+return nil, fmt.Errorf("питомец не найден")
+}
+return &pets.Pets[0], nil
 }
